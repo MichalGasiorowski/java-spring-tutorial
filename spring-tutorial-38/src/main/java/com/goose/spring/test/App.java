b20b71983b32ff9b685d5fdcc0bae6c91cce6d17
Michal Gasiorowski <enkidupal@gmail.com>
2014-07-28 16:17:27 +0200

    Working with databases, lecture 49 finished

--- spring-tutorial-38/src/main/java/com/goose/spring/test/App.java
+++ spring-tutorial-38/src/main/java/com/goose/spring/test/App.java
@@ -1,5 +1,6 @@
 package com.goose.spring.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -20,11 +21,40 @@ public class App {
 		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
 
 		try {
+			
+			//offersDao.delete(1);
+			
+			/*
+			Offer offer1 = new Offer("Dave", "dave@yoyo.com", "Coding is fun");
+			Offer offer2 = new Offer("Karen", "karen@gmail.com", "Testing for stuff");
+			
+			offersDao.create(offer1);
+			offersDao.create(offer2);
+			*/
+			List<Offer> newOffers = new ArrayList<Offer>();
+			newOffers.add(new Offer(3, "Hue", "hue@yahoo.com", "Cash for coding mad skills."));
+			newOffers.add(new Offer(2, "MaNigga", "tuPAC-nigga4life@mailinator.com", "Pimping street runners for ca$h."));
+			
+			int[] rvals = offersDao.create(newOffers);
+			for(int value: rvals) {
+				System.out.println("Updated " + value + " rows");
+			}
+			
+			Offer offer1 = new Offer(5, "Albert", "albert@cern.gov", "Banging atoms together!");
+			if(offersDao.update(offer1)) {
+				System.out.println("Object updated!");
+			} else {
+				System.out.println("Cannot update object.");
+			}
+			
 			List<Offer> offers = offersDao.getOffers();
 
 			for (Offer offer : offers) {
 				System.out.println(offer);
 			}
+			
+			Offer offer = offersDao.getOffer(2);
+			System.out.println("Should be Mike:" + offer);
 		} catch(CannotGetJdbcConnectionException ex) {
 			System.out.println("Cannot get database connection.");
 		} catch (DataAccessException ex) {
